7d11ad961cc27ecf9843e6879203762e03b5140b
Shrestha, Saujan (k21075139) <saujan.shrestha@kcl.ac.uk>
2022-10-07 05:51:47 +0100

    Add Things model and relevant tests

--- things/models.py
+++ things/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.core.validators import MinValueValidator, MaxValueValidator
 
-# Create your models here.
+class Things(models.Model):
+    name = models.CharField(
+        unique=True,
+        blank=False,
+        max_length=30
+    )
+    description = models.CharField(
+        unique=False,
+        blank=True,
+        max_length=120
+    )
+    quantity = models.IntegerField(
+        unique=False,
+        blank=False,
+        default=0,
+        validators=[MinValueValidator(0), MaxValueValidator(100)]
+    )
--- things/tests.py
+++ things/tests.py
@@ -1,3 +1,84 @@
 from django.test import TestCase
+from things.models import Things
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+class ThingModelTestCase(TestCase):
+    def setUp(self):
+        self.thing1 = Things.objects.create(
+            name="thing1",
+            description="description1",
+            quantity=1
+        )
+        self.thing2 = Things.objects.create(
+            name="thing2",
+            description="description2",
+            quantity=2
+        )
+    
+    def _assert_thing_is_valid(self):
+        try:
+            self.thing1.full_clean()
+        except (ValidationError):
+            self.fail("Test thing should be valid.")
+    
+    def _assert_thing_is_invalid(self):
+        with self.assertRaises(ValidationError):
+            self.thing1.full_clean()
+    
+    def test_valid_thing(self):
+        self._assert_thing_is_valid()
+    
+    # Name test cases.
+    def test_name_must_be_unique(self):
+        self.thing1.name = self.thing2.name
+        self._assert_thing_is_invalid()
+
+    def test_name_must_not_be_blank(self):
+        self.thing1.name = ''
+        self._assert_thing_is_invalid()
+
+    def test_name_may_be_30_characters(self):
+        self.thing1.name = 'x' * 30
+        self._assert_thing_is_valid()
+
+    def test_name_must_not_be_over_30_characters(self):
+        self.thing1.name = 'x' *31
+        self._assert_thing_is_invalid()
+    
+    # Description test cases.
+    def test_description_may_already_exist(self):
+        self.thing1.description = self.thing2.description
+        self._assert_thing_is_valid()
+
+    def test_name_may_be_blank(self):
+        self.thing1.description = ''
+        self._assert_thing_is_valid()
+
+    def test_name_may_be_120_characters(self):
+        self.thing1.description = 'x' * 120
+        self._assert_thing_is_valid()
+
+    def test_name_must_not_be_over_120_characters(self):
+        self.thing1.description = 'x' * 121
+        self._assert_thing_is_invalid()
+    
+    # Quantity test cases.
+    def test_quantity_may_already_exist(self):
+        self.thing1.quantity = self.thing2.quantity
+        self._assert_thing_is_valid()
+    
+    def test_quantity_may_be_0(self):
+        self.thing1.quantity = 0
+        self._assert_thing_is_valid()
+    
+    def test_quantity_may_be_100(self):
+        self.thing1.quantity = 100
+        self._assert_thing_is_valid()
+    
+    def test_quantity_must_not_be_under_0(self):
+        self.thing1.quantity = -1
+        self._assert_thing_is_invalid()
+    
+    def test_quantity_must_not_be_over_100(self):
+        self.thing1.quantity = 101
+        self._assert_thing_is_invalid()
